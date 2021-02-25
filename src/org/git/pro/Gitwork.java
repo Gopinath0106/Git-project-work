8bfc4302aa8ba3d5e81ce3b0a2b7635fa8c7c5d6
Gopinath0106 <gopivarma100@gmail.com>
2021-02-25 13:01:10 +0530

    gitproject

---
+++ src/org/git/pro/Gitwork.java
@@ -0,0 +1,10 @@
+package org.git.pro;
+
+public class Gitwork {
+	public static void main(String[] args) {
+System.out.println("Test1");
+System.out.println("Test2");
+System.out.println("Test3");
+	}
+
+}
---
+++ src/org/git/wor/Gitprop.java
@@ -0,0 +1,8 @@
+package org.git.wor;
+
+public class Gitprop {
+public static void main(String[] args) {
+	System.out.println("Test1");
+	System.out.println("Test3");
+}
+}
